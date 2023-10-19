5b46eb8222feece41d758d9bbceb8ca9103c14ca
Leandro Amaral <amaral_adm@hotmail.com>
2023-10-19 15:09:22 -0300

    first commit

---
+++ src/main/java/br/com/amaral/responsivewebdesigntemplates/ResponsiveWebDesignTemplatesApplication.java
@@ -0,0 +1,13 @@
+package br.com.amaral.responsivewebdesigntemplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsiveWebDesignTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponsiveWebDesignTemplatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amaral/responsivewebdesigntemplates/ResponsiveWebDesignTemplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amaral.responsivewebdesigntemplates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsiveWebDesignTemplatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
